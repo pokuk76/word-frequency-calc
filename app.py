f39633297aa1a8150219c4d677aad01191b86f6e
Kofi Poku <pokuk76@gmail.com>
2021-03-26 18:23:20 +0000

    Added routing and template for index page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app =  Flask(__name__)
@@ -12,9 +12,13 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def hello():
-	return "Hello World"
+#	@app.route('/')
+#	def hello():
+#		return "Hello World"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	return render_template('index.html')
 
 @app.route('/<name>')
 def hello_name(name):
