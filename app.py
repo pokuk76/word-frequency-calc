5add492df22feeca41960fc9c4570fc1b04c855e
Kofi Poku <pokuk76@gmail.com>
2021-03-26 21:36:38 +0000

    Handling requests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+import requests # a library for sending HTTP requests
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app =  Flask(__name__)
@@ -12,13 +13,26 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-#	@app.route('/')
-#	def hello():
-#		return "Hello World"
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	errors = []
+	results = {}
+	if request.method == "POST":
+		# get url that the user has entered
+		''' TODO: Currently user has to ensure URL includes http:// 
+		or https://. Otherwise application won’t detect 
+		that it’s a valid URL.
+		Let's fix that ^
+		'''
+		try:
+			url = request.form['url'] # Extract user-entered URL from the form object
+			r = requests.get(url) # Send a GET request to the URL
+			print(r.text)
+		except:
+			errors.append(
+				"Unable to get URL. Please make sure it's valid and try again."
+			)
+	return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
